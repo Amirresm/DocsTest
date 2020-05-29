6aa99290c2d84abf97104bf55b05f6548653a18a
Amirreza Esm <amirreza.77.esmaeili@gmail.com>
2020-05-29 16:37:37 +0430

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,10 +22,12 @@ copyright = '2020, amirreza'
 author = 'amirreza'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.0.2'
 
 
 # -- General configuration ---------------------------------------------------
+
+master_doc = 'index'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
